25d7468859a6fa5d768e3811ab7976c311458290
mackenzd <devin.p.mackenzie@gmail.com>
2024-02-08 19:47:51 -0500

    Add confirmation dialog component

---
+++ src/components/dialogs/confirmation-dialog.tsx
@@ -0,0 +1,40 @@
+export interface ConfirmationDialogProps {
+  isOpen: boolean;
+  title: string;
+  message: string;
+  onConfirm: () => void;
+  onClose: () => void;
+}
+
+const ConfirmationDialog = (props: ConfirmationDialogProps) => {
+  return props.isOpen ? (
+    <dialog className="modal" open={props.isOpen}>
+      <div className="modal-box border border-current overflow-y-visible">
+        <button
+          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          onClick={props.onClose}
+        >
+          ✕
+        </button>
+        <div className="gap-4 work-assignments-header">
+          <h3 className="font-bold text-lg">
+            {props.title}
+          </h3>
+        </div>
+        <div className="whitespace-pre-line pt-4">{props.message}</div>
+        <div className="modal-action">
+          <button className="btn btn-outline btn-sm" onClick={props.onClose}>
+            Cancel
+          </button>
+          <button className="btn btn-outline btn-sm" onClick={props.onConfirm}>
+            Confirm
+          </button>
+        </div>
+      </div>
+    </dialog>
+  ) : (
+    <></>
+  );
+};
+
+export default ConfirmationDialog;
