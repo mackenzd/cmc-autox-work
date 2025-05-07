939138fde62700df6999be59406b25242d80ea0c
Devin MacKenzie <devin.p.mackenzie@gmail.com>
2025-05-07 19:32:21 -0400

    Open results in new tab

--- src/components/events/event-results-modal.tsx
+++ src/components/events/event-results-modal.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { eventHasStarted } from "../../helpers/events";
 import {
+  useGetEventResult,
   useGetEventResults,
   useSetEventResults,
   useUnsetEventResult,
 } from "../../hooks/events";
 import { MSREvent } from "../../models/msr-event";
+import { useAuthorizationContext } from "../../contexts/authorization-context";
 
 export interface EventResultsModalProps {
   event: MSREvent;
@@ -14,11 +16,14 @@ export interface EventResultsModalProps {
 }
 
 const EventResultsModal = (props: EventResultsModalProps) => {
+  const { isAdmin } = useAuthorizationContext();
+
   const [results, setResults] = useState<string[]>([]);
 
   const getEventResults = useGetEventResults(() => {}, props.event);
   const setEventResults = useSetEventResults(() => {}, props.event);
   const unsetEventResults = useUnsetEventResult(() => {}, props.event);
+  const openResults = useGetEventResult(props.event);
 
   useEffect(() => {
     if (getEventResults) {
@@ -66,7 +71,7 @@ const EventResultsModal = (props: EventResultsModalProps) => {
         type="file"
         className="file-input file-input-primary"
         multiple
-        // disabled={!hasStarted}
+        disabled={!hasStarted}
         onChange={(e) => {
           if (e.target.files) {
             onChangeResults(e.target.files);
@@ -78,35 +83,49 @@ const EventResultsModal = (props: EventResultsModalProps) => {
     [onChangeResults, hasStarted]
   );
 
-  const resultsBadges = useMemo(() => {
-    return (
+  const preliminaryResults = useMemo(
+    () => (
       <div>
+        <div className="mt-2 mb-2">
+          {results.length > 0 ? (
+            <>These results are preliminary.</>
+          ) : (
+            <>No results to display.</>
+          )}
+        </div>
         {results.map((result) => (
           <div
             key={result}
             className="badge badge-primary gap-2 p-3 mt-1 mb-1 mr-3"
           >
-            <button onClick={() => onRemoveResult(result)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="w-4 h-4 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+            {isAdmin ? (
+              <button onClick={() => onRemoveResult(result)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="w-4 h-4 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              </button>
+            ) : (
+              <></>
+            )}
+            <button onClick={() => openResults(result)}>
+              {result.replace(/\.[^/.]+$/, "").replace(/_/g, " ")}
             </button>
-            {result}
           </div>
         ))}
       </div>
-    );
-  }, [results, onRemoveResult]);
+    ),
+    [results, openResults]
+  );
 
   return props.isOpen ? (
     <dialog className="modal" open={props.isOpen}>
@@ -121,22 +140,27 @@ const EventResultsModal = (props: EventResultsModalProps) => {
           <div className="font-bold text-lg">Event Results</div>
           <div className="font-bold text-sm">{props.event.name}</div>
         </div>
-        <div>Put Results Here</div>
-        <div className="divider divider-neutral"></div>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="font-bold label-text">
-              Upload Preliminary Results
-            </span>
-          </div>
-          {uploadPreliminaryResults}
-          <div className="label">
-            <span className="label-text-alt">
-              Upload preliminary results from AXWare.
-            </span>
-          </div>
-        </label>
-        <div className="flex flex-row mt-1">{resultsBadges}</div>
+        {preliminaryResults}
+        {isAdmin ? (
+          <>
+            <div className="divider divider-neutral"></div>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="font-bold label-text">
+                  Upload Preliminary Results
+                </span>
+              </div>
+              {uploadPreliminaryResults}
+              <div className="label">
+                <span className="label-text-alt">
+                  Upload preliminary results from AXWare.
+                </span>
+              </div>
+            </label>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </dialog>
   ) : (
