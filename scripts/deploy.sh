5b14cfb6013f417ac78a86f756d84f921a1c2fc1
Devin MacKenzie <devin.p.mackenzie@gmail.com>
2025-06-10 19:42:15 -0400

    Use docker compose v2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 DOCKER_BUILDKIT=1
 
-docker-compose -f docker-compose-production.yml up --build --detach
+docker compose -f docker-compose-production.yml up --build --detach
 docker build -f Dockerfile --target production -o /var/www/cmcwork .
